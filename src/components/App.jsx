3ebf779b79521c8fa5cf78bdc3598e6d8df8ddce
YuriySamusew <6177385@gmail.com>
2023-02-21 00:57:32 +0200

    зроблено по прикладу викладача, на основі 4 модуля серчПостс

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from 'modules/Navbar/Navbar';
+
+import HomePage from 'pages/HomePage/HomePage';
+import PostSearchPage from 'pages/PostSearchPage/PostSearchPage';
+import ContactsPage from 'pages/ContactsPage/ContactsPage';
+import NotFoundPage from 'pages/NotFoundPage/NotFoundPage';
+import SinglePostPage from 'pages/SinglePostPage/SinglePostPage';
+import SinglePostCommentPage from 'pages/SinglePostCommentPage/SinglePostCommentPage';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/posts-search" element={<PostSearchPage />}></Route>
+        <Route path="/contacts" element={<ContactsPage />}></Route>
+        <Route path="/posts/:id" element={<SinglePostPage />}>
+          <Route path="comments" element={<SinglePostCommentPage />}></Route>
+          <Route />
+        </Route>
+        <Route path="*" element={<NotFoundPage />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
